6c7c852e0a309ddf54f984944399c6c2aeaa273a
pedroanicetoo <pedro0077@gmail.com>
2022-12-27 21:13:22 -0300

    add enviroment variables

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,6 +1,7 @@
 import psycopg2
 import redis
 import json
+import os
 from crypt import methods
 from distutils.log import debug
 from bottle import Bottle, request
@@ -9,9 +10,15 @@ class Sender(Bottle):
   def __init__(self):
     super().__init__()
     self.route('/', method='POST', callback=self.send)
-    self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-    DSN = 'dbname=email_sender user=postgres password=password host=db'
-    self.conn = psycopg2.connect(DSN)
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+    db_host = os.getenv('DB_HOST', 'db')
+    db_user = os.getenv('DB_USER', 'postgres')
+    db_password = os.getenv('DB_PASSWORD', 'password')
+    db_name = os.getenv('DB_NAME', 'name')
+    dsn = f'dbname={db_name} user={db_user} password={db_password} host={db_host}'
+    self.conn = psycopg2.connect(dsn)
 
   def register_message(self, assunto, mensagem):
     SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
